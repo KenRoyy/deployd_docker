d745405e08482bcc1e8e0d67d89abb2d8e8541e1
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-04-28 19:46:14 -0500

    clase dataweb

---
+++ src/edu_pad/dataweb.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+
+
+
+class DataWeb:
+    def __init__(self):
+        self.url = "https://es.finance.yahoo.com/quote/DOLA-USD/history/"
+    
+
+    def obtener_datos(self):
+        try:
+            # url , cabeceras
+            headers = {
+                'User-Agent': 'Mozilla/5.0'
+            }
+            respuesta = requests.get(self.url,headers=headers)
+            if respuesta.status_code != 200:
+                print("La url saco error, no respondio o no existe")
+            print(respuesta.text)
+
+
+
+        except Exception as err:
+            print("Error en la funcion obtener_datos")
+
+
+
+
+dw = DataWeb()
+dw.obtener_datos()
+#print(dw.url)

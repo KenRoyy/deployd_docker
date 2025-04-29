cf8f97b9cfd7c94c744eddbb176994d28b416f69
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-04-28 20:56:01 -0500

    clase main y clase dataweb completas

---
+++ src/edu_pad/main.py
@@ -0,0 +1,16 @@
+
+from dataweb import DataWeb
+import pandas as pd
+
+
+
+def main():
+    dataweb = DataWeb()
+    df = pd.DataFrame()
+    df = dataweb.obtener_datos()
+    df.to_csv("data_web.csv")
+
+
+
+if __name__ == "__main__":
+    main()

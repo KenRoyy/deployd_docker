b1b0f88f332cd78972824bf17d1c3c028471deb3
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-05-13 01:49:51 +0000

    integracion por capas

---
+++ src/edu_pad/main_extractor.py
@@ -0,0 +1,16 @@
+
+from dataweb import DataWeb
+import pandas as pd
+
+
+
+def main_1():
+    dataweb = DataWeb()
+    df = dataweb.obtener_datos() 
+    df = dataweb.convertir_numericos(df) # capa 2 
+    df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False)
+
+
+
+if __name__ == "__main__":
+    main_1()

98d630cd22ef4b962c13069434045d23e8403538
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-06-04 02:01:21 +0000

    barra lateral

--- src/edu_pad/streamlit_app.py
+++ src/edu_pad/streamlit_app.py
@@ -5,4 +5,24 @@ import os
 
 
 
-st.set_page_config(page_title="Dashboard Indicadores Financieros",page_icon="📈",initial_sidebar_state="expanded")
+
+
+class Stramlit_app:
+    def __init__(self):
+        st.set_page_config(page_title="Dashboard Indicadores Financieros",page_icon="📈",initial_sidebar_state="expanded")
+        st.title("📈 Dashboard Indicadores Financieros")
+        self.ruta_datos="src/edu_pad/static/csv/data_extractor.csv"
+        self.df = pd.read_csv(self.ruta_datos)
+
+    def slider_bar(self):
+        with st.sidebar:
+            st.title('📈  Filtro por año')
+            
+            year_list = list(self.df.year.unique())[::-1]
+            
+            selected_year = st.selectbox('Select a year', year_list)
+            df_selected_year = self.df[self.df.year == selected_year]
+
+
+
+stramlit_app=Stramlit_app()

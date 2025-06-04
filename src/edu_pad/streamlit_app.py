f5e83756f1b2a632dc286af8b17c4a5b53a32c32
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-06-04 02:19:25 +0000

    mapa de indicadores

--- src/edu_pad/streamlit_app.py
+++ src/edu_pad/streamlit_app.py
@@ -25,6 +25,35 @@ class Stramlit_app:
             #df_selected_year = self.df[self.df.year == selected_year]
 
 
+    # Heatmap
+    def make_heatmap(self, input_y, input_x, input_color, input_color_theme):
+        st.markdown('#### Ubicacion indicador')
+        heatmap = alt.Chart(self.df).mark_rect().encode(
+                y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+                x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+                #color=alt.Color(f'max({input_color}):Q',
+                                #legend=None,
+                                #scale=alt.Scale(scheme=input_color_theme)),
+                stroke=alt.value('black'),
+                strokeWidth=alt.value(0.25),
+            ).properties(width=900
+            ).configure_axis(
+            labelFontSize=12,
+            titleFontSize=12
+            ) 
+        # height=300
+        return heatmap
+
+#with col[1]:
+    
+    
+    #choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
+    #st.plotly_chart(choropleth, use_container_width=True)
+    
+    #heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population')
+    #st.altair_chart(heatmap, use_container_width=True)
+
 
 stramlit_app=Stramlit_app()
 stramlit_app.slider_bar()
+stramlit_app.make_heatmap('year', 'country')

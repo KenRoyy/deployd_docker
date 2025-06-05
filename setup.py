6d306d53b1aedc07dd183def7c20c1d0b9573265
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-06-05 23:02:25 +0000

    ajustes

--- setup.py
+++ setup.py
@@ -8,19 +8,20 @@ setup(
     description="ETL para análisis de datos del dólar",
     py_modules=["actividad1", "actividad2"],
     install_requires=[
-        "pandas",
-        "openpyxl",
-        "requests",
-        "beautifulsoup4",
-        "altair>=5.0.0",
-        "streamlit>=1.28.0",
-        # Core
+
+        
         "streamlit-ace",
         "streamlit-discourse",
         "streamlit-disqus",
         "streamlit-elements",
         "streamlit-pandas-profiling",
         "streamlit-player",
-        "streamlit-quill"
+        "streamlit-quill",
+        "streamlit",
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoup4",
+        "altair>=5.0.0"
     ]
 )
